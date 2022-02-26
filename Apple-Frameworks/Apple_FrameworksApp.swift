4be78d4cade0637093a9353e64010341e13de359
Andres <andresr774@gmail.com>
2022-02-26 17:00:19 -0500

    Initial Commit

---
+++ Apple-Frameworks/Apple_FrameworksApp.swift
@@ -0,0 +1,17 @@
+//
+//  Apple_FrameworksApp.swift
+//  Apple-Frameworks
+//
+//  Created by Andres camilo Raigoza misas on 26/02/22.
+//
+
+import SwiftUI
+
+@main
+struct Apple_FrameworksApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
